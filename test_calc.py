3e7b99bbe5676ce46e25b17051dab24715bc8c09
Colter Snyder <programmer114@gmail.com>
2021-06-04 11:25:22 -0600

    Added Test File
    Added test file for Pytest

---
+++ test_calc.py
@@ -0,0 +1,11 @@
+  
+import calc as pc
+
+def test_add_num():
+  assert pc.add(5, 10) == 15
+def test_sub_num():
+  assert pc.subtract(20, 10) == 10
+def test_multiply_num():
+  assert pc.multiply(5, 5) == 25
+def test_divide_num():
+  assert pc.divide(100, 10) == 10
